f4693c3adae4964956a64fb62b4a15b2bb182d8c
Jack Littley <jacklittley@live.co.uk>
2020-09-20 16:36:57 +0100

    Removing unneccessary server files

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const { HOST, PORT } = require('./config');
+
+app.use(express.static('../client/build'))
+   .use(express.static('../client/public'))
+   .use((_req, res) => res.sendFile('../client/build/index.html'));
+
+app.listen(HOST, PORT, () => console.log(`server running is on ${HOST}:${PORT}`));
